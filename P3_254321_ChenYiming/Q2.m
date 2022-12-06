915d31f2c4a883550c4b12e4e299c315a9167e88
Shocky <2697121481@qq.com>
2022-12-06 21:19:31 +0800

    feat:修改函数命，新增第二题的运行文件

---
+++ P3_254321_ChenYiming/Q2.m
@@ -0,0 +1,4 @@
+m=input('Enter the first number you want to swap .\n');
+n=input('Enter the second number you want to swap .\n');
+[m,n]=swop(m,n);
+disp(['The result of swapping the two numbers is ',num2str(m),' ',num2str(n),'.']);
--- P3_254321_ChenYiming/swop.m
+++ P3_254321_ChenYiming/swop.m
@@ -1,6 +1,6 @@
 % function for Q2
 
-function [m,n]=exchange(m,n)
+function [m,n]=swop(m,n)
 t=m;
 m=n;
 n=t;
